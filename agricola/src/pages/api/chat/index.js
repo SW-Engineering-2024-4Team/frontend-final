cf15f602dd3192f638b85bd1ad15ed64a98ae21b
Soyeon <annkwon1123@gmail.com>
2024-05-13 14:33:36 +0900

    [DOCS]안쓰는폴더/파일삭제

--- agricola/src/pages/api/chat/index.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const message = req.body;
-        if (res && res.socket && res.socket.server && res.socket.server.io) {
-            res.socket.server.io.emit('message', message);
-        }
-        res.status(201).json(message);
-    }
-}
