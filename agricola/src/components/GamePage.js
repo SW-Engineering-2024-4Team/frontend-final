a0de63758c7be612ddb5713e995c39b79c31cc6d
Soyeon <annkwon1123@gmail.com>
2024-05-13 19:27:09 +0900

    [FEAT]멀티플레이어

---
+++ agricola/src/components/GamePage.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+// MUI 불러오기
+import Grid from '@mui/material/Grid'
+
+// 보드판 불러오기
+import PlayerBoard from '../../views/boards/ProfileBoard'
+import RoundBoard from '../../views/boards/RoundBoard'
+import CurrentBoard from '../../views/boards/CurrentBoard'
+import MajorBoard from '../../views/boards/MajorBoard'
+import ResourceBoard from '../../views/boards/ResourceBoard'
+import PersonalBoard from '../../views/boards/PersonalBoard';
+import OwnBoard from '../../views/boards/OwnBoard'
+import TrigerBoard from '../../views/boards/TrigerBoard'
+
+const Game = () => {
+  const cardCount = 6;
+  const row = 3;
+
+  return (
+    <div>
+      <Grid container spacing = {3}>
+        <Grid item xs >
+          <div>
+            <PlayerBoard />
+            <PlayerBoard />
+            <PlayerBoard />
+            <PlayerBoard />
+          </div>
+        </Grid>
+        <Grid item xs >
+          <div>
+            <RoundBoard cardCount={cardCount} row={row} />
+          </div>
+        </Grid>
+        <Grid item xs >
+          <div>
+            <CurrentBoard />
+          </div>
+          <div> 
+            <MajorBoard />
+          </div>
+        </Grid>
+      </Grid>    
+      <Grid container spacing = {3}>
+        <Grid item xs >
+          <ResourceBoard />
+        </Grid>
+        <Grid item xs >
+          <PersonalBoard />
+        </Grid>
+        <Grid item xs >
+          <OwnBoard />
+          <TrigerBoard />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Game;
--- agricola/src/pages/game/index.js
+++ agricola/src/pages/game/index.js
@@ -1,61 +1,33 @@
-import React from 'react';
+import * as React from "react";
+import { useRouter } from "next/router";
 
-// MUI 불러오기
+// ** MUI Components
 import Grid from '@mui/material/Grid'
 
-// 보드판 불러오기
-import PlayerBoard from '../../views/boards/ProfileBoard'
-import RoundBoard from '../../views/boards/RoundBoard'
-import CurrentBoard from '../../views/boards/CurrentBoard'
-import MajorBoard from '../../views/boards/MajorBoard'
-import ResourceBoard from '../../views/boards/ResourceBoard'
-import PersonalBoard from '../../views/boards/PersonalBoard';
-import OwnBoard from '../../views/boards/OwnBoard'
-import TrigerBoard from '../../views/boards/TrigerBoard'
+import LoginPage from "../../components/LoginPage";
 
-const Game = () => {
-  const cardCount = 6;
-  const row = 3;
+export default function Home(props) {
+  const router = useRouter();
+
+  const handleJoinRoom = (room_, name_, option) => {
+    switch (option) {
+      case "join":
+        router.push(`game/${room_}?name=${name_}`);
+        break;
+      case "tutorial":
+        router.push("tutorial");
+        break;
+    }
+  };
 
   return (
-    <div>
-      <Grid container spacing = {3}>
-        <Grid item xs >
-          <div>
-            <PlayerBoard />
-            <PlayerBoard />
-            <PlayerBoard />
-            <PlayerBoard />
-          </div>
-        </Grid>
-        <Grid item xs >
-          <div>
-            <RoundBoard cardCount={cardCount} row={row} />
-          </div>
-        </Grid>
-        <Grid item xs >
-          <div>
-            <CurrentBoard />
-          </div>
-          <div> 
-            <MajorBoard />
-          </div>
-        </Grid>
-      </Grid>    
-      <Grid container spacing = {3}>
-        <Grid item xs >
-          <ResourceBoard />
-        </Grid>
-        <Grid item xs >
-          <PersonalBoard />
-        </Grid>
-        <Grid item xs >
-          <OwnBoard />
-          <TrigerBoard />
-        </Grid>
-      </Grid>
-    </div>
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      my={20}
+    >
+      <LoginPage type="home" btnFunction={handleJoinRoom} />
+    </Grid>
   );
-};
-
-export default Game;
+}
