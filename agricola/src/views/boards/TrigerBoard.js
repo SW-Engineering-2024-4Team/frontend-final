67c7f16b659091f4b76c5d7da0d0095156cf78e4
Jeong jiyoon <59586621+j1037@users.noreply.github.com>
2024-05-18 08:23:15 +0900

    [FEAT]트리거보드 제작

--- agricola/src/views/boards/TrigerBoard.js
+++ agricola/src/views/boards/TrigerBoard.js
@@ -1,35 +1,56 @@
-import React from 'react';
-
-// MUI 불러오기
-import Card from '@mui/material/Card'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
-import CardContent from '@mui/material/CardContent'
-import { styled, useTheme } from '@mui/material/styles' 
-
-// 보드 컴포넌트 불러오기
-import Board from '../../components/Board' 
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 
-// 라운드 카드 불러오기
-import RoundCard from '../cards/RoundCard'
+import MinorCard from '../cards/MinorCard';
+import WorkCard from '../cards/WorkCard';
 
 const TrigerBoard = () => {
-   // 다크모드 세팅하기
-   const theme = useTheme()
-   const imageSrc = theme.palette.mode === 'light' ? 'triangle-light.png' : 'triangle-dark.png'
- 
+
   return (
     <Box
       height={170}
-      width={420}
-      my={4}
+      width={400}
+      my={2}
       display="flex"
-      alignItems="center"
+      alignItems="flex-start" 
+      justifyContent="flex-start"
       gap={4}
-      p={2}
-      sx={{ border: '2px solid grey' }}
+      p={4}
+      sx={{ 
+        border: '2px solid grey',
+        overflowX: 'auto',
+        overflowY: 'visible',
+        '&::-webkit-scrollbar': { display: 'none', },
+        '-ms-overflow-style': 'none',
+        'scrollbar-width': 'none',
+      }}
     >
+      <Box
+        justifyContent="flex-start" 
+        sx={{ 
+          display: 'flex', 
+          width: 'max-content', 
+        }}
+      >
+        {Array.from(Array(3)).map((_, index) => (
+          <React.Fragment key={index}>
+            <Box sx={{ flex: '0 0 auto', mr: '-30px' }}>
+              <MinorCard
+                number={index + 1}
+                index={index}
+                onClick={(index, number) => handleCardClick(index, number)}
+              />
+            </Box>
+            <Box sx={{ flex: '0 0 auto', mr: '-30px' }}>
+              <WorkCard
+                number={index + 1}
+                index={index}
+                onClick={(index, number) => handleCardClick(index, number)}
+              />
+            </Box>
+          </React.Fragment>
+        ))}
+      </Box>
     </Box>
   );
 };
